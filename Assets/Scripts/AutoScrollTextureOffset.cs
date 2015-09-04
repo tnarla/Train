56c1e94cfc7cee813ac50152d8fe0a41c8debc5a
tnarla <trushithanarla@gmail.com>
2015-09-04 05:41:46 -0500

    Initial Commit

---
+++ Assets/Scripts/AutoScrollTextureOffset.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoScrollTextureOffset : MonoBehaviour {
+
+	public Vector2 scrollSpeed = Vector2.one;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 finalTextureOffset = GetComponent<Renderer>().material.mainTextureOffset;
+		finalTextureOffset += scrollSpeed*Time.deltaTime;
+
+		if(finalTextureOffset.x >= 1.0f)
+		{
+			finalTextureOffset.x -= (float)System.Math.Truncate(finalTextureOffset.x);
+		}
+		while(finalTextureOffset.x < 0.0f)
+		{
+			finalTextureOffset.x += 1.0f;
+		}
+		if(finalTextureOffset.y >= 1.0f)
+		{
+			finalTextureOffset.y -= (float)System.Math.Truncate(finalTextureOffset.y);
+		}
+		while(finalTextureOffset.y < 0.0f)
+		{
+			finalTextureOffset.y += 1.0f;
+		}
+		
+		GetComponent<Renderer>().material.mainTextureOffset = finalTextureOffset;
+	}
+
+}
